632f0838b685efc52cd796b3ea7caa19e4f57461
Alina Malyutina <linamalutina1@gmail.com>
2022-11-10 20:31:13 +0300

    first task

---
+++ arrayposneg/Program.cs
@@ -0,0 +1,23 @@
+﻿int [] array = new int [12];
+for(int i=0;i<array.Length;i++)
+{
+    array [i] = new Random().Next(-9,10);
+    Console.Write($"{array[i]}, ");
+}
+
+int sumPos = 0;
+int sumNeg = 0;
+
+for (int i=0;i<array.Length;i++)
+{
+    if (array [i] > 0) 
+    {
+        sumPos = sumPos + array [i];
+    }
+    else
+    {
+        sumNeg = sumNeg + array [i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Sum of positive numbers in array is {sumPos}, of negative is {sumNeg}");
